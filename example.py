cb5bf3eef14b0b02b3ea8724b7aa818c2e174375
Ishan Banerjee <89222104+ishanbaner@users.noreply.github.com>
2024-08-20 23:48:30 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,8 @@
 solve=Prover()
 
+#For P o Q, the expression would be ['o','P','Q'] for o being in {'=>','and','or'}
+#For not P, the expression would be ['not','P']
+#For example
 #Assuming P => (Q => R)
 #Show: (not R) => (P => (not Q))
 
